44501c587d90136d699bffb906eae4e8fbb57264
ashmeetsingh003 <ashmeetsingh01396@gmail.com>
2022-10-17 13:32:45 -0400

    Question 3a complete

--- src/main/java/com/example/comp1011s1st200496893/CarSoldViewController.java
+++ src/main/java/com/example/comp1011s1st200496893/CarSoldViewController.java
@@ -1,41 +1,50 @@
 package com.example.comp1011s1st200496893;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class CarSoldViewController {
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class CarSoldViewController implements Initializable {
 
     @FXML
     private BarChart<?, ?> barChart;
 
     @FXML
-    private TableColumn<?, ?> carIdColumn;
+    private TableColumn<CarSold, Integer> carIdColumn;
 
     @FXML
     private CategoryAxis categoryAxis;
 
     @FXML
-    private TableColumn<?, ?> dateSoldColumn;
+    private TableColumn<CarSold, LocalDate> dateSoldColumn;
 
     @FXML
-    private TableColumn<?, ?> makeColumn;
+    private TableColumn<CarSold, String> makeColumn;
 
     @FXML
-    private TableColumn<?, ?> modelColumn;
+    private TableColumn<CarSold,String > modelColumn;
 
     @FXML
-    private TableColumn<?, ?> modelYearColumn;
+    private TableColumn<CarSold, Integer> modelYearColumn;
 
     @FXML
     private NumberAxis numberAxis;
 
     @FXML
-    private TableColumn<?, ?> priceColumn;
+    private TableColumn<CarSold,Integer > priceColumn;
 
     @FXML
     private Label totalSoldLabel;
@@ -46,4 +55,22 @@ public class CarSoldViewController {
     @FXML
     private ComboBox<?> yearCombo;
 
+    @FXML
+    private TableView<CarSold> tableView;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        carIdColumn.setCellValueFactory(new PropertyValueFactory<>("carId"));
+        modelYearColumn.setCellValueFactory(new PropertyValueFactory<>("modelYear"));
+        makeColumn.setCellValueFactory(new PropertyValueFactory<>("make"));
+        modelColumn.setCellValueFactory(new PropertyValueFactory<>("model"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        dateSoldColumn.setCellValueFactory(new PropertyValueFactory<>("dateSold"));
+
+        ArrayList<CarSold> carSold = DBUtilities.getCarSoldFromDB();
+        tableView.getItems().addAll(DBUtilities.getCarSoldFromDB());
+
+
+    }
 }
