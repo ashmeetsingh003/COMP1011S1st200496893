8c649ffa141f11cba041e650bb7ca8e2d6163569
ashmeetsingh003 <ashmeetsingh01396@gmail.com>
2022-10-17 12:19:21 -0400

    2A complete

---
+++ src/main/java/com/example/comp1011s1st200496893/DBUtilities.java
@@ -0,0 +1,51 @@
+package com.example.comp1011s1st200496893;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class DBUtilities {
+
+        private static String user = "Ashmeet200496893"; //MySQL server username
+        private static String pw = "Wd_qBy-drd";   //MySQL server password
+        private static String connectUrl = "jdbc:mysql://172.31.22.43:3306/Ashmeet200496893";
+
+    public static ArrayList<CarSold> getCarSoldFromDB()
+    {
+        ArrayList<CarSold> carsSold = new ArrayList();
+
+        String sql = "SELECT * FROM carSales;";
+
+
+        try(
+                Connection conn = DriverManager.getConnection(connectUrl,user,pw);
+                Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+        )
+        {
+
+            while (resultSet.next())
+            {
+                //creating the car object
+                int carId = resultSet.getInt("carID");
+                int modelYear = resultSet.getInt("modelYear");
+                String make = resultSet.getString("make");
+                String model = resultSet.getString("model");
+                int price = resultSet.getInt("price");
+                LocalDate dateSold = resultSet.getDate("dateSold").toLocalDate();
+
+                CarSold carSold = new CarSold(carId, modelYear,price,make,model,dateSold);
+
+                carsSold.add(carSold);
+            }
+        }
+        catch (Exception e)
+        {e.printStackTrace();}
+
+        return carsSold;
+    }
+
+
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.comp1011s1st200496893 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.comp1011s1st200496893 to javafx.fxml;
